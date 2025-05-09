587ae03f8b5377ecd8b7dd2037f1d41a365a8fc0
emilio norrmann <enorrmann@gmail.com>
2025-05-09 19:52:28 -0300

    version y reportes

--- src/main/java/org/solidstate/facturen/gui/VistaImpresion.java
+++ src/main/java/org/solidstate/facturen/gui/VistaImpresion.java
@@ -1,17 +1,23 @@
-/*   */ package org.solidstate.facturen.gui;
-/*   */ 
+/*   */
+package org.solidstate.facturen.gui;
+
+/*   */
 /*   */ import net.sf.jasperreports.engine.JRException;
 /*   */ import net.sf.jasperreports.engine.JasperPrint;
-/*   */ import net.sf.jasperreports.view.JRViewer;
-/*   */ 
+import net.sf.jasperreports.swing.JRViewer;
+
+
+/*   */
 /*   */ public class VistaImpresion extends JRViewer {
-/*   */   public VistaImpresion(JasperPrint jrPrint) throws JRException {
-/* 9 */     super(jrPrint);
-/*   */   }
-/*   */ }
+    /*   */ public VistaImpresion(JasperPrint jrPrint) throws JRException {
+        /* 9 */ super(jrPrint);
+        /*   */ }
+    /*   */ }
 
-
-/* Location:              /home/emilio/veterinaria_facturen/facturas/FacturenGui.jar!/org/solidstate/facturen/gui/VistaImpresion.class
+/*
+ * Location:
+ * /home/emilio/veterinaria_facturen/facturas/FacturenGui.jar!/org/solidstate/
+ * facturen/gui/VistaImpresion.class
  * Java compiler version: 6 (50.0)
- * JD-Core Version:       1.1.3
+ * JD-Core Version: 1.1.3
  */
